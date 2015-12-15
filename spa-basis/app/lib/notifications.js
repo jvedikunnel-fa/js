c268f97b400b973b5af7b6cf315f798f928efd49
Vedikunnel Joseph <jvedikunnel@financeactive.com>
2015-12-15 11:28:51 +0100

    Ajout d'un prompt et affichage du nom

---
+++ spa-basis/app/lib/notifications.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jvedikunnel on 15/12/2015.
+ */
+var userName = prompt("Votre nom d'utilisateur ?");
+
+exports.userName = userName;
--- spa-basis/app/views/home_view.js
+++ spa-basis/app/views/home_view.js
@@ -4,8 +4,15 @@
 'use strict';
 
 var View = require('./view');
+var userName = require('lib/notifications').userName;
+
 
 module.exports = View.extend({
   // Le template principal
-  template: require('./templates/home')
+  template: require('./templates/home'),
+  getRenderData: function homeRenderData () {
+    return {
+      userName: userName
+    };
+  }
 });
