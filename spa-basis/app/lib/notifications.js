8adf90d0083ee92f39cb4392d8e23b15c2211520
Vedikunnel Joseph <jvedikunnel@financeactive.com>
2015-12-15 12:06:03 +0100

    Ajout de la date et son actualisation toutes les secondes

--- spa-basis/app/lib/notifications.js
+++ spa-basis/app/lib/notifications.js
@@ -1,6 +1,15 @@
 /**
  * Created by jvedikunnel on 15/12/2015.
  */
-var userName = prompt("Votre nom d'utilisateur ?");
+var $ = require('jquery');
+var _ = require('underscore');
+
+var userName = sessionStorage.userName || $.trim(prompt("Votre nom d'utilisateur ?"));
+
+if (userName) {
+    sessionStorage.userName = userName;
+} else {
+    userName = "Anonymous_" + _.random(0, 1000);
+}
 
 exports.userName = userName;
--- spa-basis/app/views/home_view.js
+++ spa-basis/app/views/home_view.js
@@ -4,6 +4,7 @@
 'use strict';
 
 var View = require('./view');
+var moment = require('moment');
 var userName = require('lib/notifications').userName;
 
 
@@ -12,7 +13,21 @@ module.exports = View.extend({
   template: require('./templates/home'),
   getRenderData: function homeRenderData () {
     return {
-      userName: userName
+      userName: userName,
+      now: moment().format('dddd D MMMM YYYY HH:mm:ss')
     };
+  },
+  afterRender: function  homeAfterRender() {
+    this.startClock();
+  },
+  startClock: function startClock() {
+    var clock = this.$('#ticker');
+    var that = this;
+    setInterval(function() {
+      clock.text(that.getRenderData().now);
+    }, 1000);
+    //setInterval(function() {
+    //  clock.text(this.getRenderData().now);
+    //}.bind(this), 1000); // compatible après IE9
   }
 });
