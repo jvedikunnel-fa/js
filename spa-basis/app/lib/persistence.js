6a82eda185528ca97e2248e4d69e8a0827c09704
Vedikunnel Joseph <jvedikunnel@financeactive.com>
2015-12-16 12:04:26 +0100

    sauvegarde des checkins pending au retour de la connexion

--- spa-basis/app/lib/persistence.js
+++ spa-basis/app/lib/persistence.js
@@ -5,6 +5,7 @@
 var CheckInsCollection = require('models/collection');
 var Backbone = require('backbone');
 var cnxSvc = require('lib/connectivity');
+var _ = require('underscore');
 
 var collection = new CheckInsCollection();
 //console.log(collection);
@@ -14,11 +15,38 @@ function addCheckIn(checkIn) {
     collection.create(checkIn); // add + save
 }
 
+var pendings;
+
 function initialLoad() {
     if (!cnxSvc.isOnline()) {
         return;
     }
-    collection.fetch({reset: true}); // reset permet d'envoyer un event qu'on pourra écouter pour render la vue
+
+    pendings = collection.filter(function(m) {
+        return m.isNew();
+    });
+
+    if (pendings.length) {
+        collection.on('sync', accountForSync); // event sync envoyé par backbone quand envoyé au serveur
+        _.invoke(pendings, 'save'); // exécute le save sur chaque élément de pendings
+    } else {
+        collection.fetch({reset: true}); // reset permet d'envoyer un event qu'on pourra écouter pour render la vue
+    }
+}
+
+Backbone.Mediator.subscribe('connectivity:online', initialLoad);
+
+function accountForSync(model) {
+    // suppression de l'élément synchronisé par backbone
+    pendings = _.without(pendings, model); // recharge les pendings sans l'élément synchronisé
+    if (pendings.length) {
+        return;
+    }
+
+    collection.off('sync', accountForSync); // suppression du listener pour ne pas refaire le fetch à caque sauvegarde suivante
+
+    // il n'y en a plus : on fetch
+    collection.fetch({reset: true});
 }
 
 collection.on('reset', function(){
