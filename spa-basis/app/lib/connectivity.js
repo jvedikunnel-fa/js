bd9c61c152115e7c2b0ffabe41f313c95a535bb6
Vedikunnel Joseph <jvedikunnel@financeactive.com>
2015-12-16 10:58:42 +0100

    récupération de l'info de connectivité et hide/show de l'icone de connexion

--- spa-basis/app/lib/connectivity.js
+++ spa-basis/app/lib/connectivity.js
@@ -3,6 +3,9 @@
 
 'use strict';
 
+var $ = require('jquery');
+var Backbone = require('backbone');
+
 // Ce module encapsule la gestion de la connectivité
 // (états online/offline du navigateur) à destination des
 // divers éléments externes (notamment la couche de persistence
@@ -11,3 +14,15 @@
 // On publie une seule méthode : isOnline.  Indique le bon état en
 // live sur à peu près tous les browsers modernes.
 exports.isOnline = function() { return true; };
+
+if ('onLine' in navigator) {
+    exports.isOnline = function() { return navigator.onLine; };
+    $(window).on('online offline', checkStatus);
+    checkStatus();
+}
+
+function checkStatus() {
+    Backbone.Mediator.publish(
+        exports.isOnline() ? 'connectivity:online' : 'connectivity:offline'
+    );
+}
