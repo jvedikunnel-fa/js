61ff7e0b2e00a77ecb2a358df6f1999101e395eb
Vedikunnel Joseph <jvedikunnel@financeactive.com>
2016-01-01 16:10:58 +0100

    adding doc and exercice

---
+++ js-total-fa/spa-final/app/lib/appcache.js
@@ -0,0 +1,16 @@
+// Rechargement AppCache
+// =====================
+
+'use strict';
+
+var $ = require('jquery');
+
+// Test simple de disponibilité de la gestion AppCache
+if (window.applicationCache) {
+  // Si le navigateur nous signale en cours d'utilisation de la page qu'on
+  // vient de récupérer une nouvelle version, proposer à l'utilisateur
+  // de la charger tout de suite, au moyen d'un Modal Dialog (Bootstrap).
+  window.applicationCache.addEventListener('updateready', function() {
+    $('#reloadPrompt').modal('show');
+  });
+}
